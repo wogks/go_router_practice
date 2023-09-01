650186eacd35df91f6f51468a37b9782cb6214a1
JH <wogks27@naver.com>
2023-09-01 10:55:48 +0900

    Feat: push

--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:gorout/screens/1_basic_screen.dart';
 import 'package:gorout/screens/2_named_screen.dart';
+import 'package:gorout/screens/3_push_screen.dart';
 import 'package:gorout/screens/root_screen.dart';
 
 final router = GoRouter(
@@ -17,6 +18,10 @@ final router = GoRouter(
           path: 'named',
           name: 'named_screen',
           builder: (context, state) => const NamedScreen(),
+        ),
+        GoRoute(
+          path: 'push',
+          builder: (context, state) => const PushScreen(),
         ),
       ],
     ),
